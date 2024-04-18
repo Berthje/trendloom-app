7bb13491f3f9d7413f1db7db2735da469a6c5f16
Layton Berth <laytonbrth@gmail.com>
2024-04-18 17:08:40 +0200

    Add AnnouncementBar component to App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup>
 import { RouterView } from 'vue-router'
 import NavBar from './components/NavBar.vue'
+import AnnouncementBar from './components/AnnouncementBar.vue'
 </script>
 
 <template>
+  <AnnouncementBar />
   <NavBar />
   <RouterView />
 </template>
--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
+import './assets/reset.css'
 import './assets/main.css'
 
 import { createApp } from 'vue'
