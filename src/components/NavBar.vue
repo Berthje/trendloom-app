5a68a6fa5f59dafcae10a907fe2aeb0b108c1605
Layton Berth <laytonbrth@gmail.com>
2024-04-18 17:17:20 +0200

    Add vue3-icon dependency and import it in NavBar.vue

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,8 +1,29 @@
 <script setup>
+import { HiOutlineShoppingBag } from "vue3-icons/hi2";
 </script>
 
 <template>
-    
+    <header>
+        <nav>
+            <div>
+                <h1>TrendLoom</h1>
+                <ul>
+                    <li>Women</li>
+                    <li>Men</li>
+                    <li>Children</li>
+                    <li>Brands</li>
+                </ul>
+            </div>
+            <div>
+                <input type="search" placeholder="Search a product">
+                <div>
+                    <HiOutlineShoppingBag size="20"/>
+                    <p>0</p>
+                </div>
+                <a href="#">Login</a>
+            </div>
+        </nav>
+    </header>
 </template>
 
 <style scoped>
