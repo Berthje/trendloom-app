949f87d25d43dc232a8c98a5b6644732a92b297e
Layton Berth <laytonbrth@gmail.com>
2024-04-19 03:09:31 +0200

    Update AnnouncementBar.vue to hide on small screens

--- src/components/AnnouncementBar.vue
+++ src/components/AnnouncementBar.vue
@@ -2,7 +2,7 @@
 </script>
 
 <template>
-    <header class="bg-black text-white uppercase underline text-center px-4 py-2">
+    <header class="hidden sm:block bg-black text-white uppercase underline text-center px-4 py-2">
         <p>FREE SHIPPING fON ALL T-SHIRTS - MAY. 19–24. </p>
     </header>
 </template>
