914ea8a2ff4491809851e4046b9361dd65c392a2
Layton Berth <laytonbrth@gmail.com>
2024-04-19 03:09:39 +0200

    Import RxHamburgerMenu from vue3-icons/rx in NavBar.vue

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,30 +1,12 @@
 <script setup>
 import { HiOutlineShoppingBag } from "vue3-icons/hi2";
+import { RxHamburgerMenu } from "vue3-icons/rx";
 </script>
 
 <template>
     <header>
         <nav>
-            <div>
-                <h1>TrendLoom</h1>
-                <ul>
-                    <li>Women</li>
-                    <li>Men</li>
-                    <li>Children</li>
-                    <li>Brands</li>
-                </ul>
-            </div>
-            <div>
-                <input type="search" placeholder="Search a product">
-                <div>
-                    <HiOutlineShoppingBag size="20"/>
-                    <p>0</p>
-                </div>
-                <a href="#">Login</a>
-            </div>
         </nav>
     </header>
 </template>
-
-<style scoped>
-</style>
+<style scoped></style>
