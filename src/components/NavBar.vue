3e02b1753d8441a6877882352613cc80c0bc9826
Layton Berth <laytonbrth@gmail.com>
2024-04-22 12:22:54 +0200

    Refactor NavBar.vue to improve accessibility and fix cartOpen initial value

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -27,36 +27,38 @@ const links = [
 </script>
 
 <template>
-  <header class="bg-white w-full max-w-screen-xl py-4 px-4 mx-auto">
-    <nav class="flex items-center justify-between">
-      <HiOutlineMenu size="28" class="cursor-pointer lg:hidden" @click="toggleCart" />
-      <div class="lg:flex space-x-6">
-        <img src="../assets/logo.svg" class="w-36" alt="Trendloom Logo">
-        <nav>
-          <ul class="hidden lg:flex space-x-4 uppercase text-md font-bold">
-            <li v-for="link in links" :key="link.path">
-              <RouterLink :to="link.path" active-class="active-route">{{ link.name }}</RouterLink>
-            </li>
-          </ul>
-        </nav>
-        <AiOutlineSearch class="hidden lg:block" size="22" />
-      </div>
-      <div class="flex items-center">
-        <div class="hidden lg:flex space-x-3">
-          <GoPerson size="1.25rem" />
-          <FaRegHeart size="1.25rem" />
-          <IoLanguage size="1.25rem" />
+  <header class="bg-white sticky top-0 z-10">
+    <div class="w-full max-w-screen-xl py-4 px-4 mx-auto">
+      <nav class="flex items-center justify-between">
+        <HiOutlineMenu size="28" class="cursor-pointer lg:hidden" @click="toggleCart" />
+        <div class="lg:flex space-x-6">
+          <img src="../assets/logo.svg" class="w-36" alt="Trendloom Logo">
+          <nav>
+            <ul class="hidden lg:flex space-x-4 uppercase text-md font-bold">
+              <li v-for="link in links" :key="link.path">
+                <RouterLink :to="link.path" active-class="active-route">{{ link.name }}</RouterLink>
+              </li>
+            </ul>
+          </nav>
+          <AiOutlineSearch class="hidden lg:block" size="22" />
         </div>
-        <div class="flex items-center lg:border-l-[1px] lg:border-gray-400 lg:border-solid lg:pl-4 lg:ml-4">
-          <div class="relative inline-flex mr-2">
-            <AiOutlineShopping size="28" />
-            <span
-              class="bg-black text-white text-[0.70rem] w-4 h-4 leading-6 rounded-full inline-flex items-center justify-center font-bold absolute left-[20px]">0</span>
+        <div class="flex items-center">
+          <div class="hidden lg:flex space-x-3">
+            <GoPerson size="1.25rem" />
+            <FaRegHeart size="1.25rem" />
+            <IoLanguage size="1.25rem" />
           </div>
-          <p class="hidden lg:block ml-2 text-[0.875rem] font-bold">CART €0.00</p>
+          <div class="flex items-center lg:border-l-[1px] lg:border-gray-400 lg:border-solid lg:pl-4 lg:ml-4">
+            <div class="relative inline-flex mr-2">
+              <AiOutlineShopping size="28" />
+              <span
+                class="bg-black text-white text-[0.70rem] w-4 h-4 leading-6 rounded-full inline-flex items-center justify-center font-bold absolute left-[20px]">0</span>
+            </div>
+            <p class="hidden lg:block ml-2 text-[0.875rem] font-bold">CART €0.00</p>
+          </div>
         </div>
-      </div>
-    </nav>
+      </nav>
+    </div>
   </header>
   <transition name="fade">
     <div v-show="cartOpen" class="fixed inset-0 bg-black opacity-70 z-40 lg:hidden"></div>
@@ -64,7 +66,8 @@ const links = [
   <transition name="slide">
     <div v-show="cartOpen" class="fixed top-0 h-full w-full overflow-auto z-50 lg:hidden">
       <nav class="fixed top-0 h-full w-80 bg-white overflow-auto p-8 font-bold uppercase">
-        <RouterLink to="/" class="block mb-6"><img src="../assets/logo.svg" class="w-36 mx-auto" alt="Trendloom Logo"></RouterLink>
+        <RouterLink to="/" class="block mb-6"><img src="../assets/logo.svg" class="w-36 mx-auto" alt="Trendloom Logo">
+        </RouterLink>
         <form action="#" class="relative mb-4">
           <div class="w-full overflow-hidden text-black flex items-center border border-solid border-gray-300">
             <div class="w-full relative flex items-center">
