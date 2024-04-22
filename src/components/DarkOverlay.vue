da954b6e29bcad112b87edb8aa0a418c7732cf4e
Layton Berth <laytonbrth@gmail.com>
2024-04-22 23:43:44 +0200

    Refactor DarkOverlay.vue to add fade animation for improved user experience

--- src/components/DarkOverlay.vue
+++ src/components/DarkOverlay.vue
@@ -17,3 +17,10 @@ const close = () => {
     <div v-show="show" class="fixed inset-0 bg-black opacity-70 z-40" @click="close"></div>
   </transition>
 </template>
+
+<style scoped>
+.fade-enter,
+.fade-leave-to {
+  @apply opacity-0;
+}
+</style>
