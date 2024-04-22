d2150bad8c7a3b36a1b188c82c060c8cdb35b700
Layton Berth <laytonbrth@gmail.com>
2024-04-23 00:13:13 +0200

    Add AccountPage.vue to router configuration

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import LandingPage from '../views/LandingPage.vue'
+import AccountPage from '../views/AccountPage.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -9,6 +10,11 @@ const router = createRouter({
       name: 'home',
       component: LandingPage
     },
+    {
+      path: '/account',
+      name: 'account',
+      component: AccountPage
+    }
   ]
 })
 
