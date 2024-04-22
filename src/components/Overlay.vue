eaaf60ed3c69515d7ea7bf855bfe026b00ec1296
Layton Berth <laytonbrth@gmail.com>
2024-04-22 23:39:42 +0200

    Refactor NavBar.vue to add search functionality and improve accessibility

---
+++ src/components/Overlay.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { defineProps, defineEmits } from 'vue';
+
+const props = defineProps({
+  show: Boolean,
+});
+
+const emits = defineEmits(['close']);
+
+const close = () => {
+  emits('close');
+};
+</script>
+
+<template>
+  <transition name="fade">
+    <div v-show="show" class="fixed inset-0 bg-black opacity-70 z-40" @click="close"></div>
+  </transition>
+</template>
