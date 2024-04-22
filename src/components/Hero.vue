4afeea9d5676b1b3abaad04471ab62b72690ddf3
Layton Berth <laytonbrth@gmail.com>
2024-04-22 11:13:51 +0200

    Update Hero.vue to add a new section with a hero image and call-to-action button

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -2,6 +2,13 @@
 </script>
 
 <template>
+    <section class="bg-cover bg-center bg-hero-image flex flex-col justify-center text-white min-h-[28rem] p-6">
+        <div class="font-bold">
+            <h2 class="text-xl mb-1">Explore Styles ready for Summer Day</h2>
+            <h3 class="text-5xl uppercase font-extrabold mb-4">Summer's Wanted</h3>
+            <RouterLink to="/products" class="uppercase inline-block px-10 py-3 bg-black hover:bg-white hover:text-black transition ease-in-out duration-300">Shop now</RouterLink>
+        </div>
+    </section>
 </template>
 
 <style scoped>
