c63518103239d1bb2511a237c69e3f3bee3f7ec4
Layton Berth <laytonbrth@gmail.com>
2024-04-22 23:14:08 +0200

    Refactor NavBar.vue to fix cartOpen initial value and improve accessibility

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref } from 'vue';
+import { ref, watchEffect } from 'vue';
 import { AiOutlineShopping, AiOutlineClose, AiOutlineSearch } from "vue3-icons/ai";
 import { HiOutlineMenu } from "vue3-icons/hi";
 import { GoPerson } from "vue3-icons/go";
@@ -35,6 +35,15 @@ const links = [
   { name: 'About us', path: '/about-us' },
   { name: 'Contacts', path: '/contact' },
 ];
+
+watchEffect(() => {
+  if (cartOpen.value || searchOpen.value) {
+    document.body.classList.add('overflow-hidden');
+  } else {
+    document.body.classList.remove('overflow-hidden');
+  }
+});
+
 </script>
 
 <template>
@@ -47,11 +56,12 @@ const links = [
           <nav>
             <ul class="hidden lg:flex space-x-4 uppercase text-md font-bold">
               <li v-for="link in links" :key="link.path">
-                <RouterLink :to="link.path" class="hover:active-route transition-all duration-150" active-class="active-route">{{ link.name }}</RouterLink>
+                <RouterLink :to="link.path" class="hover:active-route transition-all duration-150"
+                  active-class="active-route">{{ link.name }}</RouterLink>
               </li>
             </ul>
           </nav>
-          <AiOutlineSearch class="hidden cursor-pointer lg:block" size="22" @click="openSearch"/>
+          <AiOutlineSearch class="hidden cursor-pointer lg:block" size="22" @click="openSearch" />
         </div>
         <div class="flex items-center">
           <div class="hidden lg:flex space-x-3">
@@ -72,7 +82,7 @@ const links = [
     </div>
   </header>
   <transition name="fade">
-    <div v-show="cartOpen || searchOpen" class="fixed inset-0 bg-black opacity-70 z-40"></div>
+    <div v-show="cartOpen || searchOpen" class="fixed inset-0 bg-black opacity-70 z-40" @click="closeCart; closeSearch;"></div>
   </transition>
   <transition name="slide">
     <div v-show="cartOpen" class="fixed top-0 h-full w-full overflow-auto z-50 lg:hidden">
@@ -122,21 +132,41 @@ const links = [
           </ul>
         </div>
       </nav>
-      <AiOutlineClose v-show="cartOpen" class="h-8 w-8 bg-white rounded-full p-2 cursor-pointer z-50 absolute right-3 top-3" @click="closeCart" />
+      <AiOutlineClose v-show="cartOpen"
+        class="h-8 w-8 bg-white rounded-full p-2 cursor-pointer z-50 absolute right-3 top-3" @click="closeCart" />
     </div>
   </transition>
-  <transition name="slide-search">
-    <div v-show="searchOpen" class="fixed top-0 h-8 w-full overflow-auto z-50 lg:hidden">
-      <AiOutlineClose v-show="searchOpen" class="h-8 w-8 bg-white rounded-full p-2 cursor-pointer z-50 absolute right-3 top-3" @click="closeSearch" />
+  <transition name="slide-down">
+    <div v-show="searchOpen" class="hidden fixed top-0 bg-white h-44 w-full py-6 overflow-auto z-50 text-center lg:block">
+      <h3 class="mb-4 font-bold text-2xl capitalize">What Are You Looking For?</h3>
+      <form action="#" class="inline-block w-[40rem] relative mb-4">
+        <div class="w-full overflow-hidden text-black flex items-center border border-solid border-gray-300">
+          <div class="w-full relative flex items-center">
+            <input type="text" class="w-full h-auto min-h-10 pl-4 pr-6 leading-8 outline-none text-start"
+              placeholder="Search for products..." autocomplete="off">
+          </div>
+          <div class="flex items-center">
+            <button class="inline-flex items-center justify-center whitespace-nowrap h-10 p-0 min-w-10" type="submit"
+              title="Search">
+              <AiOutlineSearch size="22" />
+            </button>
+          </div>
+        </div>
+      </form>
+      <div class="flex justify-center items-center space-x-4">
+        <h4 class="text-lg uppercase text-gray-500 text-[0.875rem]">Popular Searches: </h4>
+        <ul class="flex justify-center space-x-2 text-[0.925rem]">
+          <li class="bg-gray-200 px-2 cursor-pointer hover:bg-black hover:text-white transition-all">Jeans</li>
+          <li class="bg-gray-200 px-2 cursor-pointer hover:bg-black hover:text-white transition-all">T-shirts</li>
+          <li class="bg-gray-200 px-2 cursor-pointer hover:bg-black hover:text-white transition-all">Shoes</li>
+        </ul>
+      </div>
+      <AiOutlineClose v-show="searchOpen" class="h-6 w-6 cursor-pointer z-50 absolute right-3 top-3"
+        @click="closeSearch" />
     </div>
   </transition>
 </template>
 <style scoped>
-.fade-enter-active,
-.fade-leave-active {
-  @apply transition-opacity duration-300 ease-in-out;
-}
-
 .fade-enter,
 .fade-leave-to {
   @apply opacity-0;
@@ -164,5 +194,29 @@ const links = [
 
 .slide-leave-to {
   @apply -translate-x-full;
+}
+
+.slide-down-enter-from {
+  @apply -translate-y-full;
+}
+
+.slide-down-enter-active {
+  @apply transition-transform duration-300 ease-in-out;
+}
+
+.slide-down-enter-to {
+  @apply translate-y-0;
+}
+
+.slide-down-leave-from {
+  @apply translate-y-0;
+}
+
+.slide-down-leave-active {
+  @apply transition-transform duration-300 ease-in-out;
+}
+
+.slide-down-leave-to {
+  @apply -translate-y-full;
 }
 </style>
