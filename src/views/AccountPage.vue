cb3c220582cb70d7f9746955ef14cdfb02d24d57
Layton Berth <laytonbrth@gmail.com>
2024-04-23 02:21:47 +0200

    Update AccountPage.vue to add homepage link in login form

--- src/views/AccountPage.vue
+++ src/views/AccountPage.vue
@@ -12,7 +12,7 @@ import DividerWithText from '../components/DividerWithText.vue';
       <img src="../assets/account-image.jpg" alt="Man posing in clothing" class="w-full h-screen object-cover">
     </div>
     <div class="p-8 w-full md:w-1/2 lg:p-16">
-      <img src="../assets/logo.svg" class="mb-8 md:mb-12 w-full md:max-w-96 lg:max-w-[30rem]" alt="Trendloom logo">
+      <RouterLink to="/"><img src="../assets/logo.svg" class="mb-8 md:mb-12 w-full md:max-w-96 lg:max-w-[30rem]" alt="Trendloom logo"></RouterLink>
       <h2 class="text-3xl font-extrabold mb-4">Log In</h2>
       <div class="flex flex-col space-y-4">
         <InputField id="email" label="Email Address" placeholder="firstname.lastname@gmail.com"
@@ -31,8 +31,8 @@ import DividerWithText from '../components/DividerWithText.vue';
         <SocialButton platform="Google" buttonText="Log In" />
         <SocialButton platform="Facebook" buttonText="Register" />
       </div>
-      <p>Don't have an account? <RouterLink to="/register" class="text-md font-extrabold underline">Sign Up</RouterLink>
-      </p>
+      <p>Don't have an account? <RouterLink to="/register" class="text-md font-extrabold underline">Sign Up</RouterLink></p>
+      <p class="mt-4">or go back to the <RouterLink to="/" class="text-md font-extrabold underline">homepage</RouterLink></p>
     </div>
   </main>
 </template>
