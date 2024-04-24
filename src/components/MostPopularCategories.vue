f7343d44602209aaec0400be76b5da916761b646
layton <layton.berth@student.howest.be>
2024-04-24 12:33:05 +0200

    Update MostPopularCategories component styling and layout in MostPopularCategories.vue

--- src/components/MostPopularCategories.vue
+++ src/components/MostPopularCategories.vue
@@ -2,26 +2,26 @@
 </script>
 
 <template>
-    <section class="text-center py-6">
+    <section class="text-center py-6 lg:max-w-screen-xl mx-auto">
         <h3 class="text-2xl font-medium uppercase custom-underline mb-4">Most popular categories</h3>
-        <p class="text-sm text-gray-600">Explore our most popular categories filled with top-rated products chosen by our customers.</p>
-        <div class="flex flex-wrap">
-            <div class="w-1/2 lg:w-1/4 text-center">
+        <p class="text-sm text-gray-600 mb-6">Explore our most popular categories filled with top-rated products chosen by our customers.</p>
+        <div class="grid grid-cols-2 md:grid-cols-4 gap-4 lg:gap-12">
+            <div class="text-center">
                 <img src="../assets/category-women.avif" class="inline-block w-full aspect-square object-cover" alt="">
                 <h4>Fashion</h4>
                 <p>8 products</p>
             </div>
-            <div class="w-1/2 lg:w-1/4 text-center">
+            <div class="text-center">
                 <img src="../assets/category-women.avif" class="inline-block w-full aspect-square object-cover" alt="">
                 <h4>Fashion</h4>
                 <p>8 products</p>
             </div>
-            <div class="w-1/2 lg:w-1/4 text-center">
+            <div class="text-center">
                 <img src="../assets/category-women.avif" class="inline-block w-full aspect-square object-cover" alt="">
                 <h4>Fashion</h4>
                 <p>8 products</p>
             </div>
-            <div class="w-1/2 lg:w-1/4 text-center">
+            <div class="text-center">
                 <img src="../assets/category-women.avif" class="inline-block w-full aspect-square object-cover" alt="">
                 <h4>Fashion</h4>
                 <p>8 products</p>
