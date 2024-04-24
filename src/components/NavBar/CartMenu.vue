bffdd485d4ece48dc7e0990c81970e98cc04e0b3
Layton Berth <laytonbrth@gmail.com>
2024-04-24 23:06:24 +0200

    Refactor CategoryCard and CartSummary components, and update styling in MostPopularCategories.vue

--- src/components/NavBar/CartMenu.vue
+++ src/components/NavBar/CartMenu.vue
@@ -2,7 +2,53 @@
 import { AiOutlineShopping, AiOutlineClose } from "vue3-icons/ai";
 import { ref } from "vue";
 
-const products = ref([{}]);
+const products = ref(
+    [
+        {
+            imageUrl: "https://via.placeholder.com/150",
+            title: "Product 1",
+            color: "Black",
+            size: "M",
+            quantity: 3,
+            price: 410.00
+        },
+        {
+            imageUrl: "https://via.placeholder.com/150",
+            title: "Product 2",
+            color: "Blue",
+            size: "L",
+            quantity: 2,
+            price: 350.00
+        },
+        {
+            imageUrl: "https://via.placeholder.com/150",
+            title: "Product 3",
+            color: "Red",
+            size: "S",
+            quantity: 1,
+            price: 200.00
+        },
+        {
+            imageUrl: "https://via.placeholder.com/150",
+            title: "Product 4",
+            color: "Red",
+            size: "S",
+            quantity: 1,
+            price: 200.00
+        },
+        {
+            imageUrl: "https://via.placeholder.com/150",
+            title: "Product 5",
+            color: "Purple",
+            size: "S",
+            quantity: 1,
+            price: 200.00
+        },
+    ]
+);
+
+const MAX_PRODUCTS_SHOWN = 4;
+const additionalProducts = products.value.length - MAX_PRODUCTS_SHOWN;
 
 const props = defineProps({
     show: Boolean,
@@ -21,8 +67,8 @@ const close = () => {
             <nav class="fixed top-0 right-0 h-full w-[22rem] bg-white overflow-auto p-8 text-center flex flex-col">
                 <RouterLink to="/shopping-cart" class="block mb-6 overflow-hidden">
                     <AiOutlineShopping size="36" class="inline-block mb-2" />
-                    <h2 class="uppercase text-lg">Cart</h2>
-                    </RouterLink>
+                    <h2 class="uppercase text-lg">Cart ({{ products.length }} products)</h2>
+                </RouterLink>
                 <div v-if="products.length === 0">
                     <p class="text-sm mb-4">No products in the cart.</p>
                     <RouterLink to="/shop"
@@ -31,167 +77,58 @@ const close = () => {
                     </RouterLink>
                 </div>
                 <div v-else class="flex flex-col flex-grow">
-                    <ul class="overflow-y-auto flex-grow mb-0">
-                        <li
+                    <ul class="overflow-y-auto flex-grow">
+                        <li v-for="(product, index) in products.slice(0, 4)" :key="index"
                             class="relative group w-full flex text-left p-4 hover:bg-gray-100 transition-all duration-300 rounded-sm">
                             <AiOutlineClose size="16"
                                 class="absolute top-2 right-2 opacity-0 group-hover:opacity-100 transition-all duration-300 transform translate-x-2 group-hover:translate-x-0 cursor-pointer text-gray-500 hover:text-black" />
                             <RouterLink to="PRODUCT-DETAILS-PAGE" class="mr-5 w-20">
-                                <img src="https://via.placeholder.com/250" alt="Product" />
+                                <img :src="product.imageUrl" :alt="product.title" />
                             </RouterLink>
                             <div>
                                 <h4 class="leading-none mb-1">
-                                    <RouterLink to="PRODUCT-DETAILS-PAGE">Neck Sweater</RouterLink>
+                                    <RouterLink to="PRODUCT-DETAILS-PAGE">{{ product.title }}</RouterLink>
                                 </h4>
-
                                 <div class="text-[0.925rem]">
                                     <dl class="text-[#858585]">
                                         <div class="flex space-x-1">
                                             <dt>Color:</dt>
                                             <dd>
-                                                <p>Black</p>
+                                                <p>{{ product.color }}</p>
                                             </dd>
                                         </div>
                                         <div class="flex space-x-1">
                                             <dt>Size:</dt>
                                             <dd>
-                                                <p>M</p>
+                                                <p>{{ product.size }}</p>
                                             </dd>
                                         </div>
                                     </dl>
                                     <span class="block mt-2 font-medium text-[0.95rem]">
-                                        3 ×
+                                        {{ product.quantity }} ×
                                         <span>
                                             <bdi>€</bdi>
-                                            <span>410.00</span>
-                                        </span>
-                                    </span>
-                                </div>
-                            </div>
-                        </li>
-                        <li
-                            class="relative group w-full flex text-left p-4 hover:bg-gray-100 transition-all duration-300 rounded-sm">
-                            <AiOutlineClose size="16"
-                                class="absolute top-2 right-2 opacity-0 group-hover:opacity-100 transition-all duration-300 transform translate-x-2 group-hover:translate-x-0 cursor-pointer text-gray-500 hover:text-black" />
-                            <RouterLink to="PRODUCT-DETAILS-PAGE" class="mr-5 w-20">
-                                <img src="https://via.placeholder.com/250" alt="Product" />
-                            </RouterLink>
-                            <div>
-                                <h4 class="leading-none mb-1">
-                                    <RouterLink to="PRODUCT-DETAILS-PAGE">Neck Sweater</RouterLink>
-                                </h4>
-
-                                <div class="text-[0.925rem]">
-                                    <dl class="text-[#858585]">
-                                        <div class="flex space-x-1">
-                                            <dt>Color:</dt>
-                                            <dd>
-                                                <p>Black</p>
-                                            </dd>
-                                        </div>
-                                        <div class="flex space-x-1">
-                                            <dt>Size:</dt>
-                                            <dd>
-                                                <p>M</p>
-                                            </dd>
-                                        </div>
-                                    </dl>
-                                    <span class="block mt-2 font-medium text-[0.95rem]">
-                                        3 ×
-                                        <span>
-                                            <bdi>€</bdi>
-                                            <span>410.00</span>
-                                        </span>
-                                    </span>
-                                </div>
-                            </div>
-                        </li>
-                        <li
-                            class="relative group w-full flex text-left p-4 hover:bg-gray-100 transition-all duration-300 rounded-sm">
-                            <AiOutlineClose size="16"
-                                class="absolute top-2 right-2 opacity-0 group-hover:opacity-100 transition-all duration-300 transform translate-x-2 group-hover:translate-x-0 cursor-pointer text-gray-500 hover:text-black" />
-                            <RouterLink to="PRODUCT-DETAILS-PAGE" class="mr-5 w-20">
-                                <img src="https://via.placeholder.com/250" alt="Product" />
-                            </RouterLink>
-                            <div>
-                                <h4 class="leading-none mb-1">
-                                    <RouterLink to="PRODUCT-DETAILS-PAGE">Neck Sweater</RouterLink>
-                                </h4>
-
-                                <div class="text-[0.925rem]">
-                                    <dl class="text-[#858585]">
-                                        <div class="flex space-x-1">
-                                            <dt>Color:</dt>
-                                            <dd>
-                                                <p>Black</p>
-                                            </dd>
-                                        </div>
-                                        <div class="flex space-x-1">
-                                            <dt>Size:</dt>
-                                            <dd>
-                                                <p>M</p>
-                                            </dd>
-                                        </div>
-                                    </dl>
-                                    <span class="block mt-2 font-medium text-[0.95rem]">
-                                        3 ×
-                                        <span>
-                                            <bdi>€</bdi>
-                                            <span>410.00</span>
-                                        </span>
-                                    </span>
-                                </div>
-                            </div>
-                        </li>
-                        <li
-                            class="relative group w-full flex text-left p-4 hover:bg-gray-100 transition-all duration-300 rounded-sm">
-                            <AiOutlineClose size="16"
-                                class="absolute top-2 right-2 opacity-0 group-hover:opacity-100 transition-all duration-300 transform translate-x-2 group-hover:translate-x-0 cursor-pointer text-gray-500 hover:text-black" />
-                            <RouterLink to="PRODUCT-DETAILS-PAGE" class="mr-5 w-20">
-                                <img src="https://via.placeholder.com/250" alt="Product" />
-                            </RouterLink>
-                            <div>
-                                <h4 class="leading-none mb-1">
-                                    <RouterLink to="PRODUCT-DETAILS-PAGE">Neck Sweater</RouterLink>
-                                </h4>
-
-                                <div class="text-[0.925rem]">
-                                    <dl class="text-[#858585]">
-                                        <div class="flex space-x-1">
-                                            <dt>Color:</dt>
-                                            <dd>
-                                                <p>Black</p>
-                                            </dd>
-                                        </div>
-                                        <div class="flex space-x-1">
-                                            <dt>Size:</dt>
-                                            <dd>
-                                                <p>M</p>
-                                            </dd>
-                                        </div>
-                                    </dl>
-                                    <span class="block mt-2 font-medium text-[0.95rem]">
-                                        3 ×
-                                        <span>
-                                            <bdi>€</bdi>
-                                            <span>410.00</span>
+                                            <span>{{ product.price.toFixed(2) }}</span>
                                         </span>
                                     </span>
                                 </div>
                             </div>
                         </li>
                     </ul>
+                    <p v-if="additionalProducts > 0" class="text-left mb-32 font-bold">..there {{ additionalProducts === 1 ? 'is' : 'are'}} {{ additionalProducts }} more product{{ additionalProducts > 1 ? 's' : '' }}</p>
                     <div>
                         <div class="uppercase flex justify-between items-center mb-5">
                             <p class="text-[0.95rem]">Subtotal:</p>
                             <p>€1,565.50</p>
                         </div>
-                        <RouterLink to="checkout" class="block uppercase bg-black text-white text-center py-3 text-[0.95rem]">Checkout</RouterLink>
+                        <RouterLink to="checkout"
+                            class="block uppercase bg-black text-white text-center py-3 text-[0.95rem]">Checkout
+                        </RouterLink>
                     </div>
                 </div>
             </nav>
             <AiOutlineClose v-show="show"
-                class="h-8 w-8 bg-white rounded-full p-2 cursor-pointer z-50 absolute right-[23.5rem] top-3"
+                class="h-8 w-8 bg-white rounded-full p-2 cursor-pointer z-50 absolute right-[23rem] top-3"
                 @click="close" />
         </div>
     </transition>
