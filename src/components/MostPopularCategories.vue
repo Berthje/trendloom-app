2bd4892106402e522a3ac41a8d996286f6f527bc
layton <layton.berth@student.howest.be>
2024-04-24 12:35:06 +0200

    Update MostPopularCategories component styling and layout in MostPopularCategories.vue

--- src/components/MostPopularCategories.vue
+++ src/components/MostPopularCategories.vue
@@ -7,24 +7,32 @@
         <p class="text-sm text-gray-600 mb-6">Explore our most popular categories filled with top-rated products chosen by our customers.</p>
         <div class="grid grid-cols-2 md:grid-cols-4 gap-4 lg:gap-12">
             <div class="text-center">
-                <img src="../assets/category-women.avif" class="inline-block w-full aspect-square object-cover" alt="">
-                <h4>Fashion</h4>
-                <p>8 products</p>
+                <img src="../assets/category-women.avif" class="inline-block w-full aspect-square object-cover mb-2" alt="Category related image">
+                <div>
+                    <h4 class="uppercase font-bold text-lg">Fashion</h4>
+                    <p class="text-sm">8 products</p>
+                </div>
             </div>
             <div class="text-center">
-                <img src="../assets/category-women.avif" class="inline-block w-full aspect-square object-cover" alt="">
-                <h4>Fashion</h4>
-                <p>8 products</p>
+                <img src="../assets/category-women.avif" class="inline-block w-full aspect-square object-cover mb-2" alt="Category related image">
+                <div>
+                    <h4 class="uppercase font-bold text-lg">Fashion</h4>
+                    <p class="text-sm">8 products</p>
+                </div>
             </div>
             <div class="text-center">
-                <img src="../assets/category-women.avif" class="inline-block w-full aspect-square object-cover" alt="">
-                <h4>Fashion</h4>
-                <p>8 products</p>
+                <img src="../assets/category-women.avif" class="inline-block w-full aspect-square object-cover mb-2" alt="Category related image">
+                <div>
+                    <h4 class="uppercase font-bold text-lg">Fashion</h4>
+                    <p class="text-sm">8 products</p>
+                </div>
             </div>
             <div class="text-center">
-                <img src="../assets/category-women.avif" class="inline-block w-full aspect-square object-cover" alt="">
-                <h4>Fashion</h4>
-                <p>8 products</p>
+                <img src="../assets/category-women.avif" class="inline-block w-full aspect-square object-cover mb-2" alt="Category related image">
+                <div>
+                    <h4 class="uppercase font-bold text-lg">Fashion</h4>
+                    <p class="text-sm">8 products</p>
+                </div>
             </div>
         </div>
     </section>
