1ddf17427e415f5d85a2843b8dea94aac71158ec
layton <layton.berth@student.howest.be>
2024-04-24 12:27:18 +0200

    Add MostPopularCategories component to MostPopularCategories.vue

--- src/components/MostPopularCategories.vue
+++ src/components/MostPopularCategories.vue
@@ -5,6 +5,28 @@
     <section class="text-center py-6">
         <h3 class="text-2xl font-medium uppercase custom-underline mb-4">Most popular categories</h3>
         <p class="text-sm text-gray-600">Explore our most popular categories filled with top-rated products chosen by our customers.</p>
+        <div class="flex flex-wrap">
+            <div class="w-1/2 lg:w-1/4 text-center">
+                <img src="../assets/category-women.avif" class="inline-block w-full aspect-square object-cover" alt="">
+                <h4>Fashion</h4>
+                <p>8 products</p>
+            </div>
+            <div class="w-1/2 lg:w-1/4 text-center">
+                <img src="../assets/category-women.avif" class="inline-block w-full aspect-square object-cover" alt="">
+                <h4>Fashion</h4>
+                <p>8 products</p>
+            </div>
+            <div class="w-1/2 lg:w-1/4 text-center">
+                <img src="../assets/category-women.avif" class="inline-block w-full aspect-square object-cover" alt="">
+                <h4>Fashion</h4>
+                <p>8 products</p>
+            </div>
+            <div class="w-1/2 lg:w-1/4 text-center">
+                <img src="../assets/category-women.avif" class="inline-block w-full aspect-square object-cover" alt="">
+                <h4>Fashion</h4>
+                <p>8 products</p>
+            </div>
+        </div>
     </section>
 </template>
 
