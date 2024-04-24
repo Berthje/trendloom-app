a98c8542130568c380d84ab3087a2e748e53995c
Layton Berth <laytonbrth@gmail.com>
2024-04-24 23:11:53 +0200

    Refactor CartMenu.vue to use ProductItem component for displaying cart items

--- src/components/NavBar/CartMenu.vue
+++ src/components/NavBar/CartMenu.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { AiOutlineShopping, AiOutlineClose } from "vue3-icons/ai";
 import { ref } from "vue";
+import ProductItem from "../ProductItem.vue"
 
 const products = ref(
     [
@@ -78,44 +79,11 @@ const close = () => {
                 </div>
                 <div v-else class="flex flex-col flex-grow">
                     <ul class="overflow-y-auto flex-grow">
-                        <li v-for="(product, index) in products.slice(0, 4)" :key="index"
-                            class="relative group w-full flex text-left p-4 hover:bg-gray-100 transition-all duration-300 rounded-sm">
-                            <AiOutlineClose size="16"
-                                class="absolute top-2 right-2 opacity-0 group-hover:opacity-100 transition-all duration-300 transform translate-x-2 group-hover:translate-x-0 cursor-pointer text-gray-500 hover:text-black" />
-                            <RouterLink to="PRODUCT-DETAILS-PAGE" class="mr-5 w-20">
-                                <img :src="product.imageUrl" :alt="product.title" />
-                            </RouterLink>
-                            <div>
-                                <h4 class="leading-none mb-1">
-                                    <RouterLink to="PRODUCT-DETAILS-PAGE">{{ product.title }}</RouterLink>
-                                </h4>
-                                <div class="text-[0.925rem]">
-                                    <dl class="text-[#858585]">
-                                        <div class="flex space-x-1">
-                                            <dt>Color:</dt>
-                                            <dd>
-                                                <p>{{ product.color }}</p>
-                                            </dd>
-                                        </div>
-                                        <div class="flex space-x-1">
-                                            <dt>Size:</dt>
-                                            <dd>
-                                                <p>{{ product.size }}</p>
-                                            </dd>
-                                        </div>
-                                    </dl>
-                                    <span class="block mt-2 font-medium text-[0.95rem]">
-                                        {{ product.quantity }} ×
-                                        <span>
-                                            <bdi>€</bdi>
-                                            <span>{{ product.price.toFixed(2) }}</span>
-                                        </span>
-                                    </span>
-                                </div>
-                            </div>
-                        </li>
+                        <ProductItem v-for="(product, index) in products.slice(0, 4)" :key="index" :product="product" />
                     </ul>
-                    <p v-if="additionalProducts > 0" class="text-left mb-32 font-bold">..there {{ additionalProducts === 1 ? 'is' : 'are'}} {{ additionalProducts }} more product{{ additionalProducts > 1 ? 's' : '' }}</p>
+                    <p v-if="additionalProducts > 0" class="text-left mb-32 font-bold">..there {{ additionalProducts ===
+                        1 ? 'is' : 'are'}} {{ additionalProducts }} more product{{ additionalProducts > 1 ? 's' : '' }}
+                    </p>
                     <div>
                         <div class="uppercase flex justify-between items-center mb-5">
                             <p class="text-[0.95rem]">Subtotal:</p>
---
+++ src/components/ProductItem.vue
@@ -0,0 +1,46 @@
+<script setup>
+import { AiOutlineClose } from 'vue3-icons';
+import { RouterLink } from 'vue-router';
+
+const props = defineProps({
+    product: Object
+});
+</script>
+
+<template>
+    <li class="relative group w-full flex text-left p-4 hover:bg-gray-100 transition-all duration-300 rounded-sm">
+        <AiOutlineClose size="16"
+            class="absolute top-2 right-2 opacity-0 group-hover:opacity-100 transition-all duration-300 transform translate-x-2 group-hover:translate-x-0 cursor-pointer text-gray-500 hover:text-black" />
+        <RouterLink to="PRODUCT-DETAILS-PAGE" class="mr-5 w-20">
+            <img :src="product.imageUrl" :alt="product.title" />
+        </RouterLink>
+        <div>
+            <h4 class="leading-none mb-1">
+                <RouterLink to="PRODUCT-DETAILS-PAGE">{{ product.title }}</RouterLink>
+            </h4>
+            <div class="text-[0.925rem]">
+                <dl class="text-[#858585]">
+                    <div class="flex space-x-1">
+                        <dt>Color:</dt>
+                        <dd>
+                            <p>{{ product.color }}</p>
+                        </dd>
+                    </div>
+                    <div class="flex space-x-1">
+                        <dt>Size:</dt>
+                        <dd>
+                            <p>{{ product.size }}</p>
+                        </dd>
+                    </div>
+                </dl>
+                <span class="block mt-2 font-medium text-[0.95rem]">
+                    {{ product.quantity }} ×
+                    <span>
+                        <bdi>€</bdi>
+                        <span>{{ product.price.toFixed(2) }}</span>
+                    </span>
+                </span>
+            </div>
+        </div>
+    </li>
+</template>
