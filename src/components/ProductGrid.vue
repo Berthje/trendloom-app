bf02e25f864b7e6097ab0cf0f73b30a01080a7ad
Layton Berth <laytonbrth@gmail.com>
2024-04-25 16:30:33 +0200

    Update ProductGrid.vue to remove quantity input

--- src/components/ProductGrid.vue
+++ src/components/ProductGrid.vue
@@ -44,30 +44,7 @@
                 <RouterLink to="/brands/$BrandName"></RouterLink>
                 <h3>$155.50</h3>
                 <div>
-                    <div>
-                        <div class="relative flex items-center max-w-[8rem]">
-                            <button type="button" id="decrement-button" data-input-counter-decrement="quantity-input"
-                                class="hover:bg-gray-200 border border-gray-300 p-1 h-9 focus:ring-gray-100 focus:ring-2 focus:outline-none">
-                                <svg class="w-2 h-2 text-black" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
-                                    fill="none" viewBox="0 0 18 2">
-                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
-                                        stroke-width="1" d="M1 1h16" />
-                                </svg>
-                            </button>
-                            <input type="text" id="quantity-input" data-input-counter
-                                aria-describedby="helper-text-explanation"
-                                class="border-x-0 border-y border-gray-300 h-9 text-center text-gray-900 text-sm focus:ring-blue-500 focus:border-blue-500 block w-10 py-2.5"
-                                placeholder="999" required />
-                            <button type="button" id="increment-button" data-input-counter-increment="quantity-input"
-                            class="hover:bg-gray-200 border border-gray-300 p-1 h-9 focus:ring-gray-100 focus:ring-2 focus:outline-none">
-                                <svg class="w-2 h-2 text-black" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
-                                    fill="none" viewBox="0 0 18 18">
-                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
-                                        stroke-width="1" d="M9 1v16M1 9h16" />
-                                </svg>
-                            </button>
-                        </div>
-                    </div>
+                    
                 </div>
             </div>
         </div>
