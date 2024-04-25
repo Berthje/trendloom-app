2469d2c1dd8bb4f318899e5db6fd4a039dbc0fc7
Layton Berth <laytonbrth@gmail.com>
2024-04-25 15:45:16 +0200

    Fix missing space in ShopHeader.vue navigation links

--- src/components/ShopHeader.vue
+++ src/components/ShopHeader.vue
@@ -8,7 +8,7 @@ const props = defineProps({
     <div>
         <nav class="text-lg flex justify-center space-x-2">
             <RouterLink v-for="(link, index) in links" :key="index" :to="link.path"
-                class="[&:not(:last-child)]:after:content-['>'] hover:text-gray-400">{{ link.name }}</RouterLink>
+                class="[&:not(:last-child)]:after:content-['>'] hover:text-gray-400">{{ link.name }}&nbsp;</RouterLink>
         </nav>
         <h3 class="font-extrabold text-7xl uppercase">{{ shop }}</h3>
     </div>
