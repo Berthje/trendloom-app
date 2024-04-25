2ae8059972f83cce01c236c28815b0ff6267fca9
Layton Berth <laytonbrth@gmail.com>
2024-04-25 15:50:36 +0200

    Update ShopHeader.vue to fix navigation links and improve header styling

--- src/components/ShopHeader.vue
+++ src/components/ShopHeader.vue
@@ -5,11 +5,21 @@ const props = defineProps({
 });
 </script>
 <template>
-    <div>
-        <nav class="text-lg flex justify-center space-x-2">
-            <RouterLink v-for="(link, index) in links" :key="index" :to="link.path"
-                class="[&:not(:last-child)]:after:content-['>'] hover:text-gray-400">{{ link.name }}&nbsp;</RouterLink>
-        </nav>
-        <h3 class="font-extrabold text-7xl uppercase">{{ shop }}</h3>
-    </div>
+    <section
+        class="relative bg-category-men-header-image bg-cover bg-center w-full h-60 md:h-80 lg:h-96 text-white font-bold flex justify-center items-center text-center">
+        <div class="rotate-180 overflow-hidden left-0 bottom-0 w-full absolute">
+            <svg class="h-14 w-full" fill="white" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1000 100"
+                preserveAspectRatio="none">
+                <path d="M500,98.9L0,6.1V0h1000v6.1L500,98.9z"></path>
+            </svg>
+        </div>
+        <div>
+            <nav class="text-lg flex justify-center space-x-2">
+                <RouterLink v-for="(link, index) in links" :key="index" :to="link.path"
+                    class="[&:not(:last-child)]:after:content-['>'] hover:text-gray-400">{{ link.name }}&nbsp;
+                </RouterLink>
+            </nav>
+            <h3 class="font-extrabold text-7xl uppercase">{{ shop }}</h3>
+        </div>
+    </section>
 </template>
--- src/views/MenPage.vue
+++ src/views/MenPage.vue
@@ -1,18 +1,11 @@
 <script setup>
 import ShopHeader from '@/components/ShopHeader.vue';
+import FilterBar from '@/components/FilterBar.vue';
 </script>
 
 <template>
     <main>
-        <section
-            class="relative bg-category-men-header-image bg-cover bg-center w-full h-60 md:h-80 lg:h-96 text-white font-bold flex justify-center items-center text-center">
-            <div class="rotate-180 overflow-hidden left-0 bottom-0 w-full absolute">
-                <svg class="h-14 w-full" fill="white" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1000 100"
-                    preserveAspectRatio="none">
-                    <path d="M500,98.9L0,6.1V0h1000v6.1L500,98.9z"></path>
-                </svg>
-            </div>
-            <ShopHeader shop="Men's Shop" :links="[{ name: 'Home', path: '/' }, { name: 'Men', path: 'men' }]" />
-        </section>
+        <ShopHeader shop="Men's Shop" :links="[{ name: 'Home', path: '/' }, { name: 'Men', path: 'men' }]" />
+        <FilterBar />
     </main>
 </template>
