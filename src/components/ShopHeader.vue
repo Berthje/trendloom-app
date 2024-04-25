f3fb112ae37e591c35aae5166f2a8a0bd444fcd4
Layton Berth <laytonbrth@gmail.com>
2024-04-25 15:40:55 +0200

    Update MenPage.vue to improve header styling and navigation, and add ShopHeader component

---
+++ src/components/ShopHeader.vue
@@ -0,0 +1,16 @@
+<script setup>
+const props = defineProps({
+  title: String
+});
+</script>
+
+<template>
+    <div>
+        <nav class="text-lg flex justify-center space-x-2">
+            <RouterLink to="home" class="[&:not(:last-child)]:after:content-['>'] hover:text-gray-400">Home
+            </RouterLink>
+            <RouterLink to="home" class="[&:not(:last-child)]:after:content-['>'] hover:text-gray-400">Home</RouterLink>
+        </nav>
+        <h3 class="font-extrabold text-7xl uppercase">{{ title }}</h3>
+    </div>
+</template>
