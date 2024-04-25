9ec8f06b12ac976d69641aa8c355953463cfbb82
Layton Berth <laytonbrth@gmail.com>
2024-04-25 15:36:19 +0200

    Update MenPage.vue to improve header styling and navigation

--- src/views/MenPage.vue
+++ src/views/MenPage.vue
@@ -3,18 +3,20 @@
 
 <template>
     <main>
-        <section class="relative bg-category-men-header-image bg-cover bg-center w-full">
-            <div class="rotate-180 overflow-hidden left-0 w-full absolute">
+        <section
+            class="relative bg-category-men-header-image bg-cover bg-center w-full h-56 text-white font-bold flex justify-center items-center text-center">
+            <div class="rotate-180 overflow-hidden left-0 bottom-0 w-full absolute">
                 <svg class="h-14 w-full" fill="white" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1000 100"
                     preserveAspectRatio="none">
                     <path d="M500,98.9L0,6.1V0h1000v6.1L500,98.9z"></path>
                 </svg>
             </div>
             <div>
-                <nav>
-                    <RouterLink to="home">Home ></RouterLink>
+                <nav class="text-lg flex justify-center space-x-2">
+                    <RouterLink to="home" class="[&:not(:last-child)]:after:content-['>'] hover:text-gray-400">Home </RouterLink>
+                    <RouterLink to="home" class="[&:not(:last-child)]:after:content-['>'] hover:text-gray-400">Home</RouterLink>
                 </nav>
-                <h3>Shop</h3>
+                <h3 class="font-extrabold text-7xl uppercase">Shop</h3>
             </div>
         </section>
     </main>
