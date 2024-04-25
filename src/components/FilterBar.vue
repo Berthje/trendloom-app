26853ed944e0d2c39eab097e6b42c9c05d701a22
Layton Berth <laytonbrth@gmail.com>
2024-04-25 15:55:07 +0200

    Update FilterBar.vue to add sorting options in form

--- src/components/FilterBar.vue
+++ src/components/FilterBar.vue
@@ -2,5 +2,17 @@
 </script>
 
 <template>
-    <div></div>
+    <div>
+        <form class="max-w-sm mx-auto">
+            <label for="countries" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Select an
+                option</label>
+            <select id="countries"
+                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                <option value="default" selected>Default sorting</option>
+                <option value="latest">Sort by latest</option>
+                <option value="lth">Sort by price: low to high</option>
+                <option value="htl">Sort by price: high to low</option>
+            </select>
+        </form>
+    </div>
 </template>
