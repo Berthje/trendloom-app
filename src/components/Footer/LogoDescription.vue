368a62cc89c7353ce64c654336e17950061edfda
Layton Berth <laytonbrth@gmail.com>
2024-04-27 00:13:04 +0200

    Update LogoDescription.vue to use options API instead of composition API

--- src/components/Footer/LogoDescription.vue
+++ src/components/Footer/LogoDescription.vue
@@ -1,9 +1,15 @@
-<script setup>
+<script>
 import SocialMediaLinks from './SocialMediaLinks.vue';
+
+export default {
+    components: {
+        SocialMediaLinks
+    }
+}
 </script>
 <template>
     <div class="w-full mb-8 lg:w-1/5">
-        <img src="../../assets/logo-white.svg" class="w-40 mb-4" alt="logo" v-once/>
+        <img src="../../assets/logo-white.svg" class="w-40 mb-4" alt="logo" v-once />
         <p class="text-[#CDCDCD] text-[0.95rem] mb-4">A best collection made entirely recycled and sustainable.</p>
         <SocialMediaLinks />
     </div>
