33ef9e2ffec5d446074c44d0850761e52545ba51
Layton Berth <laytonbrth@gmail.com>
2024-04-27 00:32:31 +0200

    Refactor BrandsOverviewPage.vue to use options API instead of composition API

--- src/views/BrandsOverviewPage.vue
+++ src/views/BrandsOverviewPage.vue
@@ -1,34 +1,44 @@
-<script setup>
+<script>
 import ShopHeader from '@/components/ShopHeader.vue'
 import BrandCard from '@/components/BrandCard.vue'
 
-const brands = [
-  {
-    id: 1,
-    name: 'Gucci',
-    image: 'https://via.placeholder.com/150?text=Gucci'
+export default {
+  components: {
+    ShopHeader,
+    BrandCard
   },
-  {
-    id: 2,
-    name: 'Prada',
-    image: 'https://via.placeholder.com/150?text=Prada'
-  },
-  {
-    id: 3,
-    name: 'Versace',
-    image: 'https://via.placeholder.com/150?text=Versace'
-  },
-  {
-    id: 4,
-    name: 'Armani',
-    image: 'https://via.placeholder.com/150?text=Armani'
-  },
-  {
-    id: 5,
-    name: 'Dolce & Gabbana',
-    image: 'https://via.placeholder.com/150?text=D%26G'
-  },
-]
+  data() {
+    return {
+      brands: [
+        {
+          id: 1,
+          name: 'Gucci',
+          image: 'https://via.placeholder.com/150?text=Gucci'
+        },
+        {
+          id: 2,
+          name: 'Prada',
+          image: 'https://via.placeholder.com/150?text=Prada'
+        },
+        {
+          id: 3,
+          name: 'Versace',
+          image: 'https://via.placeholder.com/150?text=Versace'
+        },
+        {
+          id: 4,
+          name: 'Armani',
+          image: 'https://via.placeholder.com/150?text=Armani'
+        },
+        {
+          id: 5,
+          name: 'Dolce & Gabbana',
+          image: 'https://via.placeholder.com/150?text=D%26G'
+        },
+      ]
+    }
+  }
+}
 </script>
 
 <template>
@@ -36,7 +46,8 @@ const brands = [
     <ShopHeader title="Our Brands" :links="[{ name: 'Home', path: '/' }, { name: 'Brands', path: '/brands' }]" />
     <div
       class="px-3 py-8 grid grid-cols-2 max-w-screen-xl mx-auto sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-y-4 justify-center">
-      <BrandCard v-for="brand in brands" :key="brand.id" :brandName="brand.name" :brandImage="brand.image" :brandId="brand.id" />
+      <BrandCard v-for="brand in brands" :key="brand.id" :brandName="brand.name" :brandImage="brand.image"
+        :brandId="brand.id" />
     </div>
   </main>
 </template>
