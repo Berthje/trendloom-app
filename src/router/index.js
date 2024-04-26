6d3634b9f20e0156c906b2b0a8d60470d853cd06
Layton Berth <laytonbrth@gmail.com>
2024-04-26 18:53:30 +0200

    Add brand detail route to router

--- src/router/index.js
+++ src/router/index.js
@@ -31,6 +31,11 @@ const router = createRouter({
             component: BrandsOverviewPage,
         },
         {
+            path: "/brands/:id",
+            name: "brandDetail",
+            component: BrandsOverviewPage,
+        },
+        {
             path: "/login",
             name: "login",
             component: LoginPage,
