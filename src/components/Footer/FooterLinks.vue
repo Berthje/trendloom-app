467aac1c99e0811398722cf522ff96163166244d
Layton Berth <laytonbrth@gmail.com>
2024-04-27 00:18:11 +0200

    Update FooterLinks.vue to fix prop references in template

--- src/components/Footer/FooterLinks.vue
+++ src/components/Footer/FooterLinks.vue
@@ -20,9 +20,9 @@ export default {
 
 <template>
     <div class="w-1/2 pb-12 lg:w-1/5">
-        <h3 class="uppercase font-home font-extrabold pb-4">{{ props.title }}</h3>
+        <h3 class="uppercase font-home font-extrabold pb-4">{{ title }}</h3>
         <ul class="flex flex-col space-y-2 text-[0.925rem]">
-            <li class="hover:text-gray-400" v-for="link in props.links" :key="link.text">
+            <li class="hover:text-gray-400" v-for="link in links" :key="link.text">
                 <a v-if="link.href" :href="link.href">{{ link.text }}</a>
                 <RouterLink v-else-if="link.to" :to="link.to">{{ link.text }}</RouterLink>
             </li>
