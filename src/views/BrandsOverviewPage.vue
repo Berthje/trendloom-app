30af8a1950b0a5b43710862a591d4a5a2abed88f
Layton Berth <laytonbrth@gmail.com>
2024-04-26 19:23:20 +0200

    Add brand ID prop to BrandCard component in BrandsOverviewPage.vue

--- src/views/BrandsOverviewPage.vue
+++ src/views/BrandsOverviewPage.vue
@@ -36,7 +36,7 @@ const brands = [
     <ShopHeader title="Our Brands" :links="[{ name: 'Home', path: '/' }, { name: 'Brands', path: '/brands' }]" />
     <div
       class="px-3 py-8 grid grid-cols-2 max-w-screen-xl mx-auto sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-y-4 justify-center">
-      <BrandCard v-for="brand in brands" :key="brand.id" :brandName="brand.name" :brandImage="brand.image" />
+      <BrandCard v-for="brand in brands" :key="brand.id" :brandName="brand.name" :brandImage="brand.image" :brandId="brand.id" />
     </div>
   </main>
 </template>
