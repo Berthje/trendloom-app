483850b9170b0ccebd7e92487808004199b04450
Layton Berth <laytonbrth@gmail.com>
2024-04-26 15:17:04 +0200

    Update ShopHeader.vue and ProductDetailPage.vue to improve code readability and fix styling issues

--- src/components/ShopHeader.vue
+++ src/components/ShopHeader.vue
@@ -8,7 +8,7 @@ const props = defineProps({
     <section
         class="relative bg-category-men-header-image bg-cover bg-center w-full h-60 md:h-80 lg:h-96 text-white font-bold flex justify-center items-center text-center">
         <div class="rotate-180 overflow-hidden left-0 bottom-0 w-full absolute">
-            <svg class="h-14 w-full" fill="white" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1000 100"
+            <svg class="h-8 md:h-12 lg:h-14 w-full" fill="white" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1000 100"
                 preserveAspectRatio="none">
                 <path d="M500,98.9L0,6.1V0h1000v6.1L500,98.9z"></path>
             </svg>
@@ -19,7 +19,7 @@ const props = defineProps({
                     class="[&:not(:last-child)]:after:content-['>'] hover:text-gray-400">{{ link.name }}&nbsp;
                 </RouterLink>
             </nav>
-            <h3 class="font-extrabold text-7xl uppercase">{{ title }}</h3>
+            <h3 class="font-extrabold text-4xl md:text-5xl lg:text-6xl uppercase">{{ title }}</h3>
         </div>
     </section>
 </template>
--- src/views/ProductDetailPage.vue
+++ src/views/ProductDetailPage.vue
@@ -26,13 +26,13 @@ onMounted(async () => {
 <template>
     <main>
         <ShopHeader title="Cotton Crew Neck" :links="[{ name: 'Home', path: '/' }, { name: 'Shop', path: '/shop' }]" />
-        <section>
-        <div>
-            <img :src="mainImage" alt="Main product" class="w-full h-auto">
-            <div class="flex overflow-x-scroll mt-4 space-x-4">
-                <img v-for="(imageURL, index) in tempImages" :key="index" :src="imageURL" class="w-24 h-24 cursor-pointer" @click="mainImage = imageURL" alt="Product">
+        <section class="px-4 py-4">
+            <div>
+                <img :src="mainImage" alt="Main product" class="w-full h-auto">
+                <div class="flex overflow-x-scroll mt-4 space-x-4">
+                    <img v-for="(imageURL, index) in tempImages" :key="index" :src="imageURL" class="w-24 h-24 cursor-pointer" @click="mainImage = imageURL" alt="Product">
+                </div>
             </div>
-        </div>
-    </section>
+        </section>
     </main>
 </template>
