dddd43f0ca7ebdffe4aa604c1fe39f4e9c22c917
Layton Berth <laytonbrth@gmail.com>
2024-04-26 19:29:20 +0200

    Fix router links in BrandCard.vue, CartMenuFooter.vue, and ProductCard.vue

--- src/components/BrandCard.vue
+++ src/components/BrandCard.vue
@@ -7,7 +7,7 @@ const props = defineProps({
 </script>
 
 <template>
-    <RouterLink :to="`brands/${brandId}`" class="block mx-auto">
+    <RouterLink :to="`/brands/${brandId}`" class="block mx-auto">
         <img :src="brandImage" :alt="brandName" />
         <h2 class="text-center mt-1">{{ brandName }}</h2>
     </RouterLink>
--- src/components/CartMenu/CartMenuFooter.vue
+++ src/components/CartMenu/CartMenuFooter.vue
@@ -6,7 +6,7 @@
             <p class="text-[0.95rem]">Subtotal:</p>
             <p>€1,565.50</p>
         </div>
-        <RouterLink to="checkout" class="block uppercase bg-black text-white text-center py-3 text-[0.95rem]">Checkout
+        <RouterLink to="/checkout" class="block uppercase bg-black text-white text-center py-3 text-[0.95rem]">Checkout
         </RouterLink>
     </div>
 </template>
