fce132a44ae7d40a0e0df6c2645d2d9240b6028f
Layton Berth <laytonbrth@gmail.com>
2024-04-27 00:11:05 +0200

    Update CartMenuHeader.vue to use options API instead of composition API

--- src/components/CartMenu/CartMenuHeader.vue
+++ src/components/CartMenu/CartMenuHeader.vue
@@ -1,14 +1,24 @@
-<script setup>
+<script>
 import { AiOutlineShopping } from "vue3-icons/ai";
 import { RouterLink } from 'vue-router';
 
-const props = defineProps({
-    amountOfProducts: Number
-});
+export default {
+    components: {
+        AiOutlineShopping,
+        RouterLink
+    },
+    props: {
+        amountOfProducts: {
+            type: Number,
+            required: true
+        }
+    }
+}
 </script>
 <template>
     <RouterLink to="/shopping-cart" class="block mb-6 overflow-hidden">
         <AiOutlineShopping size="36" class="inline-block mb-2" />
-        <h2 class="uppercase text-lg">Cart {{ amountOfProducts > 0 ? `(${amountOfProducts} ${amountOfProducts > 1 ? 'products' : 'product'})` : '' }}</h2>
+        <h2 class="uppercase text-lg">Cart {{ amountOfProducts > 0 ? `(${amountOfProducts} ${amountOfProducts > 1 ?
+            'products' : 'product'})` : '' }}</h2>
     </RouterLink>
 </template>
