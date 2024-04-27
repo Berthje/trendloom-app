6048608493aa1be825d67913862601fcb292f5bb
layton <layton.berth@student.howest.be>
2024-04-27 22:44:36 +0200

    Update API endpoint in MenPageService.js to fetch products from the "Men" category

--- src/modules/MenPage/Services/MenPageService.js
+++ src/modules/MenPage/Services/MenPageService.js
@@ -2,7 +2,7 @@ import { BASE_URL } from "../../Core/config";
 
 export default class MenPageService {
     async allProducts(filterOptions) {
-        const url = new URL(`${BASE_URL}/products`);
+        const url = new URL(`${BASE_URL}/categories/2/products`);
         url.searchParams.append("sort", filterOptions.sorting);
         url.searchParams.append("itemCount", filterOptions.itemCount);
 
