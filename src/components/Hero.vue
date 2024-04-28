6b32454e853faa7d236420b7ebd7186fd2d11fec
layton <layton.berth@student.howest.be>
2024-04-29 00:57:22 +0200

    Update shop link in Hero.vue and remove ChildrenPage.vue

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -6,7 +6,7 @@
         <div class="font-bold">
             <h2 class="text-xl md:text-2xl mb-1">Explore Styles ready for Summer Day</h2>
             <h3 class="text-5xl md:text-7xl lg:text-8xl uppercase font-extrabold mb-4">Summer’s Wanted</h3>
-            <RouterLink to="/products" class="uppercase inline-block px-10 py-3 bg-black hover:bg-white hover:text-black transition ease-in-out duration-300">Shop now</RouterLink>
+            <RouterLink to="/shop" class="uppercase inline-block px-10 py-3 bg-black hover:bg-white hover:text-black transition ease-in-out duration-300">Shop now</RouterLink>
         </div>
         <div class="hidden lg:block rotate-180 overflow-hidden left-0 w-full absolute -bottom-[1px]">
             <svg class="h-14 w-full" fill="white" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1000 100" preserveAspectRatio="none">
