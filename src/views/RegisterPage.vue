f2c3e34645fe63f1337bd7fbdfc72a910a211c27
layton <layton.berth@student.howest.be>
2024-04-29 20:31:31 +0200

    Add input validation for first name, last name, and email in RegisterPage.vue

--- src/views/RegisterPage.vue
+++ src/views/RegisterPage.vue
@@ -90,16 +90,16 @@ export default {
                 <div class="flex flex-col sm:flex-row space-y-4 sm:space-y-0 sm:space-x-4">
                     <InputField id="firstname" label="First Name" placeholder="John"
                         errorMessage="The first name field is required." :status="firstNameStatus" type="text"
-                        class="sm:w-1/2" v-model="firstName"/>
+                        class="sm:w-1/2" v-model="firstName" @input="validateFirstName"/>
                     <InputField id="lastname" label="Last Name" placeholder="Doe"
-                        errorMessage="The last name field is required." :status="lastNameStatus" type="text" class="sm:w-1/2" v-model="lastName"
+                        errorMessage="The last name field is required." :status="lastNameStatus" type="text" class="sm:w-1/2" v-model="lastName" @input="validateLastName"
                     />
                 </div>
                 <InputField id="email" label="Email Address" placeholder="firstname.lastname@gmail.com"
-                    errorMessage="The email must be a valid email address." :status="emailStatus" type="email" v-model="email"/>
+                    errorMessage="The email must be a valid email address." :status="emailStatus" type="email" v-model="email" @input="validateEmail"/>
                 <InputField id="password" label="Password" placeholder="your password"
                     errorMessage="It must be a combination of minimum 8 letters, numbers, and symbols." :status="passwordStatus"
-                    type="password" v-model="password"/>
+                    type="password" v-model="password" @input="validatePassword"/>
                 <button
                     class="w-full block border-solid bg-black border-2 border-black text-white py-2 hover:bg-white hover:text-black hover:border-black" @click="register">Register
                     my account
