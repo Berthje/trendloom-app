5bd2e94612882e67af3948e60c2b4e47bc8d8cc3
layton <layton.berth@student.howest.be>
2024-04-30 19:45:45 +0200

    Refactor ProfileForm.vue to improve layout and structure

--- src/components/ProfileForm.vue
+++ src/components/ProfileForm.vue
@@ -16,12 +16,22 @@ export default {
 </script>
 <template>
     <div>
-        <form class="border-b border-solid border-black pb-6 mb-4 flex flex-col space-y-3">
-            <InputField v-for="(field, index) in fields" :key="index" :id="field.id" :label="field.label"
-                :placeholder="field.placeholder" :errorMessage="field.errorMessage" :status="field.status"
-                :type="field.type" />
-            <button
-                class="uppercase inline-block px-12 py-2 bg-black hover:bg-white hover:text-black transition ease-in-out duration-300 text-white">Save</button>
-        </form>
+      <h2 class="py-2">Personal Details</h2>
+      <form class="border-b border-solid border-black pb-6 flex flex-wrap -mx-2">
+        <div class="w-full md:w-1/2 px-2 mb-4" v-for="(field, index) in fields" :key="index">
+          <InputField
+            :id="field.id"
+            :label="field.label"
+            :placeholder="field.placeholder"
+            :errorMessage="field.errorMessage"
+            :status="field.status"
+            :type="field.type"
+          />
+        </div>
+        <div class="w-full px-2">
+          <button
+            class="uppercase inline-block px-12 py-2 bg-black hover:bg-white hover:text-black transition ease-in-out duration-300 text-white">Save</button>
+        </div>
+      </form>
     </div>
-</template>
+  </template>
