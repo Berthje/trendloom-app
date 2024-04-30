c7bfd4c415e753ec93a4346f755b77459fe5d931
layton <layton.berth@student.howest.be>
2024-04-30 19:42:22 +0200

    Refactor InputField.vue and AccountProfile.vue components

---
+++ src/components/ProfileForm.vue
@@ -0,0 +1,27 @@
+<script>
+import InputField from './InputField.vue';
+
+export default {
+    name: 'ProfileForm',
+    components: {
+        InputField
+    },
+    props: {
+        fields: {
+            type: Array,
+            required: true
+        }
+    }
+}
+</script>
+<template>
+    <div>
+        <form class="border-b border-solid border-black pb-6 mb-4 flex flex-col space-y-3">
+            <InputField v-for="(field, index) in fields" :key="index" :id="field.id" :label="field.label"
+                :placeholder="field.placeholder" :errorMessage="field.errorMessage" :status="field.status"
+                :type="field.type" />
+            <button
+                class="uppercase inline-block px-12 py-2 bg-black hover:bg-white hover:text-black transition ease-in-out duration-300 text-white">Save</button>
+        </form>
+    </div>
+</template>
--- src/views/AccountProfile.vue
+++ src/views/AccountProfile.vue
@@ -1,12 +1,43 @@
 <script>
+import InputField from '@/components/InputField.vue'
+import ProfileForm from '@/components/ProfileForm.vue';
 
 export default {
   name: 'AccountProfile',
+  components: {
+    InputField,
+    ProfileForm
+  },
+  data() {
+    return {
+      personalDetailFields: [
+        { id: 'firstname', label: 'First Name', placeholder: 'John', errorMessage: 'The first name field is required.', status: 'default', type: 'text' },
+        { id: 'lastname', label: 'Last Name', placeholder: 'Doe', errorMessage: 'The last name field is required.', status: 'default', type: 'text' },
+        { id: 'email', label: 'Email', placeholder: 'firstname.lastname@gmail.com', errorMessage: 'The email should be valid.', status: 'default', type: 'email' },
+        { id: 'phone', label: 'Phone number', placeholder: '+12 345 67 89 00', errorMessage: 'The phone number should be valid.', status: 'default', type: 'tel' }
+      ],
+      passwordFields: [
+        { id: 'oldPassword', label: 'Old Password', placeholder: 'Enter your old password', errorMessage: 'The old password field is required.', status: 'default', type: 'password' },
+        { id: 'newPassword', label: 'New Password', placeholder: 'Enter your new password', errorMessage: 'The new password field is required.', status: 'default', type: 'password' },
+      ],
+      addressFields: [
+        { id: 'address', label: 'Address', placeholder: '123 Main St', errorMessage: 'The address field is required.', status: 'default', type: 'text' },
+        { id: 'type', label: 'Type (e.g. Residential, Business)', placeholder: 'Residential, Business', errorMessage: '', status: 'default', type: 'text' },
+        { id: 'zipcode', label: 'Zip Code', placeholder: '12345', errorMessage: 'The zip code field is required.', status: 'default', type: 'text' },
+        { id: 'city', label: 'City', placeholder: 'City', errorMessage: 'The city field is required.', status: 'default', type: 'text' },
+        { id: 'state', label: 'State', placeholder: 'State', errorMessage: 'The state field is required.', status: 'default', type: 'text' },
+        { id: 'country', label: 'Country', placeholder: 'Country', errorMessage: 'The country field is required.', status: 'default', type: 'text' },
+      ]
+    }
+  }
 }
 </script>
 
 <template>
   <main>
-    accountprofile
+    <h1 class="font-bold text-2xl pb-2 border-b border-solid border-black ">My Profile</h1>
+    <ProfileForm :fields="personalDetailFields" />
+    <ProfileForm :fields="passwordFields" />
+    <ProfileForm :fields="addressFields" />
   </main>
 </template>
