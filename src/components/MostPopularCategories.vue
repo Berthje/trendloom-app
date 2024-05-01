3c2cc619d08b22b157920dfc4ac98dbc144a6d2e
Layton Berth <laytonbrth@gmail.com>
2024-05-01 22:12:59 +0200

    Update MostPopularCategories.vue to include dynamic categories data

--- src/components/MostPopularCategories.vue
+++ src/components/MostPopularCategories.vue
@@ -5,22 +5,27 @@ export default {
     name: 'MostPopularCategories',
     components: {
         CategoryCard
+    },
+    data() {
+        return {
+            categories: [
+                { imageSrc: "/images/fashion.jpeg", altText: "Category related", title: "Fashion", productCount: "281" },
+                { imageSrc: "/images/accessories.jpeg", altText: "Category related", title: "Accessories", productCount: "124" },
+                { imageSrc: "/images/featured.jpeg", altText: "Category related", title: "Featured", productCount: "327" },
+                { imageSrc: "/images/men.jpeg", altText: "Category related", title: "Men", productCount: "102" },
+            ]
+        }
     }
 }
 </script>
 
 <template>
     <section class="text-center py-6 lg:max-w-screen-xl mx-auto">
-        <h3 class="text-2xl font-medium uppercase custom-underline mb-4">{{$t('most_popular_categories')}}</h3>
-        <p class="text-sm text-gray-600 mb-6">{{$t('explore_popular_categories')}}</p>
+        <h3 class="text-2xl font-medium uppercase custom-underline mb-4">{{ $t('most_popular_categories') }}</h3>
+        <p class="text-sm text-gray-600 mb-6">{{ $t('explore_popular_categories') }}</p>
         <div class="grid grid-cols-2 md:grid-cols-4 gap-4 lg:gap-12">
-            <CategoryCard imageSrc="/images/fashion.jpeg" altText="Category related" title="Fashion"
-                productCount="281" />
-            <CategoryCard imageSrc="/images/accessories.jpeg" altText="Category related" title="Accessories"
-                productCount="124" />
-            <CategoryCard imageSrc="/images/featured.jpeg" altText="Category related" title="Featured"
-                productCount="327" />
-            <CategoryCard imageSrc="/images/men.jpeg" altText="Category related" title="Men" productCount="102" />
+            <CategoryCard v-for="(category, index) in categories" :key="index" :imageSrc="category.imageSrc"
+                :altText="category.altText" :title="category.title" :productCount="category.productCount" />
         </div>
     </section>
 </template>
