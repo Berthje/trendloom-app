19127c1c6abfc04cc29a457f6239e185876cb9cf
Layton Berth <laytonbrth@gmail.com>
2024-05-01 17:19:07 +0200

    Update language modal styling and functionality in LanguageModal.vue and NavBar.vue

--- src/components/LanguageModal.vue
+++ src/components/LanguageModal.vue
@@ -30,13 +30,13 @@ export default {
 <template>
     <div v-show="show" class="fixed top-0 h-full w-full overflow-auto z-50 flex justify-center items-center">
         <div class="bg-white w-full h-full">
-            <div class="border-b border-solid border-gray-300 py-3">
-                <AiOutlineClose class="h-6 w-6 bg-white cursor-pointer ml-auto" @click="close" />
+            <div class="border-b border-solid border-gray-300 p-3">
+                <AiOutlineClose class="h-6 w-6 bg-white cursor-pointer ml-auto hover:opacity-40" @click="close" />
             </div>
-            <div>
-                <h2>Which language would you like us to use?</h2>
-                <div v-for="lang in languages" :key="lang.id" class="mb-2">
-                    <input type="radio" :id="lang" :value="lang" v-model="selectedLanguage" />
+            <div class="p-6">
+                <h2 class="text-2xl font-extrabold mb-8">Which language would you like us to use?</h2>
+                <div v-for="lang in languages" :key="lang" class="mb-2">
+                    <input type="radio" :id="lang" :value="lang" name="language" class="radio-button" />
                     <label :for="lang">{{ lang }}</label>
                 </div>
             </div>
@@ -47,3 +47,17 @@ export default {
         </div>
     </div>
 </template>
+
+<style scoped>
+.radio-button {
+  @apply appearance-none w-5 h-5 border-2 border-black rounded-full outline-none cursor-pointer relative;
+}
+
+.radio-button:checked {
+  @apply bg-white;
+}
+
+.radio-button:checked::after {
+    @apply content-[''] block w-3 h-3 bg-black rounded-full absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2;
+}
+</style>
