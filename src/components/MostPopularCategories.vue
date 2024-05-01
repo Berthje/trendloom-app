9bfecabff2c5bc92d2d9d649864a4943a7a95794
Layton Berth <laytonbrth@gmail.com>
2024-05-01 22:15:10 +0200

    Update MostPopularCategories.vue to include dynamic categories data

--- src/components/MostPopularCategories.vue
+++ src/components/MostPopularCategories.vue
@@ -6,14 +6,10 @@ export default {
     components: {
         CategoryCard
     },
-    data() {
-        return {
-            categories: [
-                { imageSrc: "/images/fashion.jpeg", altText: "Category related", title: "Fashion", productCount: "281" },
-                { imageSrc: "/images/accessories.jpeg", altText: "Category related", title: "Accessories", productCount: "124" },
-                { imageSrc: "/images/featured.jpeg", altText: "Category related", title: "Featured", productCount: "327" },
-                { imageSrc: "/images/men.jpeg", altText: "Category related", title: "Men", productCount: "102" },
-            ]
+    props: {
+        categories: {
+            type: Array,
+            required: true
         }
     }
 }
--- src/views/LandingPage.vue
+++ src/views/LandingPage.vue
@@ -10,6 +10,16 @@ export default {
     CategoriesOverview,
     MostPopularCategories
   },
+  data() {
+    return {
+      categories: [
+        { imageSrc: "/images/fashion.jpeg", altText: "Category related", title: "Fashion", productCount: "281" },
+        { imageSrc: "/images/accessories.jpeg", altText: "Category related", title: "Accessories", productCount: "124" },
+        { imageSrc: "/images/featured.jpeg", altText: "Category related", title: "Featured", productCount: "327" },
+        { imageSrc: "/images/men.jpeg", altText: "Category related", title: "Men", productCount: "102" },
+      ]
+    }
+  }
 }
 </script>
 
@@ -18,7 +28,7 @@ export default {
     <Hero />
     <div class="px-3 py-6">
       <CategoriesOverview />
-      <MostPopularCategories />
+      <MostPopularCategories :categories="categories"/>
     </div>
   </main>
 </template>
