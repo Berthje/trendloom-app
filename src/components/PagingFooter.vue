80267f7bf8a8d789aec272968f4538d1752232ac
Layton Berth <laytonbrth@gmail.com>
2024-05-01 15:12:50 +0200

    Update PagingFooter.vue to improve pagination button alignment

--- src/components/PagingFooter.vue
+++ src/components/PagingFooter.vue
@@ -1,28 +1,3 @@
-<template>
-    <div class="flex items-center gap-4">
-        <button :disabled="!prevPageUrl" @click.prevent="goToPage(prevPageUrl)"
-            class="flex items-center gap-2 px-6 py-3 -ml-6 font-sans text-xs font-bold text-center text-gray-900 uppercase align-middle transition-all rounded-lg select-none hover:bg-gray-900/10 active:bg-gray-900/20 disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
-            type="button">
-            Previous
-        </button>
-        <div class="flex items-center gap-2">
-            <a v-for="(link, index) in pageLinks" :key="link.label" @click.prevent="goToPage(link.url)" :class="[
-                'cursor-pointer relative h-10 max-h-[40px] w-10 max-w-[40px] select-none rounded-lg text-center align-middle font-sans text-xs font-medium uppercase transition-all hover:bg-gray-900/10 active:bg-gray-900/20 disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none',
-                { 'bg-gray-900 text-white': link.active }
-            ]">
-                <span class="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2">
-                    {{ link.label }}
-                </span>
-            </a>
-        </div>
-        <button :disabled="!nextPageUrl" @click.prevent="goToPage(nextPageUrl)"
-            class="flex items-center gap-2 px-6 py-3 font-sans text-xs font-bold text-center text-gray-900 uppercase align-middle transition-all rounded-lg select-none hover:bg-gray-900/10 active:bg-gray-900/20 disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
-            type="button">
-            Next
-        </button>
-    </div>
-</template>
-
 <script>
 export default {
     name: 'PagingFooter',
@@ -52,3 +27,28 @@ export default {
     },
 };
 </script>
+<template>
+    <div class="flex items-center gap-4 justify-center">
+        <button :disabled="!prevPageUrl" @click.prevent="goToPage(prevPageUrl)"
+            class="flex items-center gap-2 px-6 py-3 font-sans text-xs font-bold text-center text-gray-900 uppercase align-middle transition-all rounded-lg select-none hover:bg-gray-900/10 active:bg-gray-900/20 disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+            type="button">
+            Previous
+        </button>
+        <div class="flex items-center gap-2 overflow-x-auto hide-scrollbar">
+            <a v-for="(link, index) in pageLinks" :key="link.label" @click.prevent="goToPage(link.url)" :class="[
+                'cursor-pointer relative h-10 max-h-[40px] w-10 max-w-[40px] select-none rounded-lg text-center align-middle font-sans text-xs font-medium uppercase transition-all hover:bg-gray-900/10 active:bg-gray-900/20 disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none',
+                { 'bg-gray-900 text-white': link.active },
+                { 'hidden sm:inline-block': index > 0 && index < pageLinks.length - 1 && !link.active }
+            ]">
+                <span class="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2">
+                    {{ link.label }}
+                </span>
+            </a>
+        </div>
+        <button :disabled="!nextPageUrl" @click.prevent="goToPage(nextPageUrl)"
+            class="flex items-center gap-2 px-6 py-3 font-sans text-xs font-bold text-center text-gray-900 uppercase align-middle transition-all rounded-lg select-none hover:bg-gray-900/10 active:bg-gray-900/20 disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+            type="button">
+            Next
+        </button>
+    </div>
+</template>
