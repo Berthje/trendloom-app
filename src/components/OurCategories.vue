36ac7cd3f22cf07ed548700fc8b88f9b27ff2880
Layton Berth <laytonbrth@gmail.com>
2024-05-01 23:38:58 +0200

    Update MostPopularCategories.vue to use OurCategories component and remove unused code

---
+++ src/components/OurCategories.vue
@@ -0,0 +1,45 @@
+<script>
+import CategoryCard from './CategoryCard.vue';
+
+export default {
+    name: 'OurCategories',
+    components: {
+        CategoryCard
+    },
+    props: {
+        categories: {
+            type: Array,
+            required: true
+        }
+    },
+    data() {
+        return {
+            categoryRoutes: {
+                'men': 'men',
+                'mannen': 'men',
+                'women': 'women',
+                'vrouwen': 'women',
+                'kids': 'kids',
+                'kinderen': 'kids',
+            }
+        }
+    },
+}
+</script>
+
+<template>
+    <section class="text-center py-6 lg:max-w-screen-xl mx-auto">
+        <h3 class="text-2xl font-medium uppercase custom-underline mb-4">{{ $t('our_categories') }}</h3>
+        <p class="text-sm text-gray-600 mb-6">{{ $t('explore_popular_categories') }}</p>
+        <div class="grid grid-cols-2 md:grid-cols-3 gap-4 lg:gap-12">
+            <CategoryCard v-for="(category, index) in categories" :key="index" :imageSrc="category.media.image_url"
+                :altText="category.description" :title="category.name" :route="`/${categoryRoutes[category.name.toLowerCase()]}`"  />
+        </div>
+    </section>
+</template>
+
+<style scoped>
+.custom-underline {
+    @apply after:block after:w-12 after:h-0.5 after:bg-black after:mx-auto;
+}
+</style>
--- src/views/LandingPage.vue
+++ src/views/LandingPage.vue
@@ -1,7 +1,7 @@
 <script>
 import Hero from '@/components/Hero.vue';
 import CategoriesOverview from '@/components/CategoriesOverview.vue';
-import MostPopularCategories from '@/components/MostPopularCategories.vue';
+import OurCategories from '@/components/OurCategories.vue';
 import LandingPageService from '@/modules/LandingPage/Services/LandingPageService';
 
 export default {
@@ -9,13 +9,13 @@ export default {
   components: {
     Hero,
     CategoriesOverview,
-    MostPopularCategories
+    OurCategories
   },
   data() {
     return {
       service: new LandingPageService(),
       categories: [],
-      allowedCategories: ['men', 'mannen', 'women', 'vrouwen', 'kids', 'kinderen', 'schoenen', 'shoes']
+      allowedCategories: ['men', 'mannen', 'women', 'vrouwen', 'kids', 'kinderen']
     }
   },
   created() {
@@ -36,7 +36,7 @@ export default {
     <Hero />
     <div class="px-3 py-6">
       <CategoriesOverview />
-      <MostPopularCategories :categories="categories" />
+      <OurCategories :categories="categories" />
     </div>
   </main>
 </template>
