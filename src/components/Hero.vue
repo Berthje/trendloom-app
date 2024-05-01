e4e78a736721909224cc8c1844fa82457007fa24
Layton Berth <laytonbrth@gmail.com>
2024-05-01 18:45:08 +0200

    Update Hero.vue to remove dynamic category description

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -4,7 +4,7 @@
 <template>
     <section class="bg-cover bg-center bg-hero-image flex flex-col justify-center text-white min-h-[28rem] lg:min-h-[38rem] p-6 lg:p-16 relative">
         <div class="font-bold">
-            <h2 class="text-xl md:text-2xl mb-1">Explore Styles ready for Summer Day {{ $t('categories_description') }}</h2>
+            <h2 class="text-xl md:text-2xl mb-1">Explore Styles ready for Summer Day</h2>
             <h3 class="text-5xl md:text-7xl lg:text-8xl uppercase font-extrabold mb-4">Summer’s Wanted</h3>
             <RouterLink to="/shop" class="uppercase inline-block px-10 py-3 bg-black hover:bg-white hover:text-black transition ease-in-out duration-300">Shop now</RouterLink>
         </div>
