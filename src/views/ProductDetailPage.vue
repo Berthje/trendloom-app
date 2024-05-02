a08637e300376e06f93f3dfdb9f990361f770d77
Layton Berth <laytonbrth@gmail.com>
2024-05-02 17:03:02 +0200

    Update quantity parameter name in updateQuantity method of ProductDetailPage.vue

--- src/views/ProductDetailPage.vue
+++ src/views/ProductDetailPage.vue
@@ -33,8 +33,8 @@ export default {
         }
     },
     methods: {
-        updateQuantity(quantity) {
-            this.quantity = quantity;
+        updateQuantity(newQuantity) {
+            this.quantity = newQuantity;
         },
         nextImage() {
             this.updateImageIndex(1);
