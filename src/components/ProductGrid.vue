52de37c171f64706c853113f422356fd506e1a42
Layton Berth <laytonbrth@gmail.com>
2024-05-03 15:34:25 +0200

    refactor: Update ProductGrid.vue to handle empty product list

--- src/components/ProductGrid.vue
+++ src/components/ProductGrid.vue
@@ -13,7 +13,9 @@ export default {
 </script>
 
 <template>
-    <section class="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 pt-5 pb-16">
+    <section v-if="products && products.length > 0"
+        class="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 pt-5 pb-16">
         <ProductCard v-for="(product, index) in products" :key="index" :product="product" />
     </section>
+    <p v-else class="text-lg py-8 text-gray-500">{{ $t('no_products_found') }}</p>
 </template>
