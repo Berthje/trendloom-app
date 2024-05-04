3224038b77d54078dff29df189776067d6f5737d
Layton Berth <laytonbrth@gmail.com>
2024-05-04 17:52:23 +0200

    refactor: Update AdminBrandsPage.vue with components and functionality

--- src/views/AdminBrandsPage.vue
+++ src/views/AdminBrandsPage.vue
@@ -1,11 +1,65 @@
 <script>
+import PageHeader from '@/components/AdminDashboard/PageHeader.vue';
+import OverviewTable from '@/components/AdminDashboard/OverviewTable.vue';
+import Pagination from '@/components/AdminDashboard/Pagination.vue';
+import AdminBrandsPageService from '@/modules/AdminBrandsPage/Services/AdminBrandsPageService';
+
 export default {
     name: 'AdminBrandsPage',
+    components: {
+        PageHeader,
+        OverviewTable,
+        Pagination
+    },
+    data() {
+        return {
+            service: new AdminBrandsPageService(),
+            brands: [],
+            paginationLinks: [],
+            filterOptions: {
+                sorting: 'default',
+                itemCount: '9'
+            },
+            headers: [
+                { key: 'media', text: 'Logo' },
+                { key: 'name', text: 'Name' },
+                { key: 'description', text: 'Description' },
+            ],
+        }
+    },
+    watch: {
+        filterOptions(newValue, oldValue) {
+            this.fetchBrands();
+        }
+    },
+    created() {
+        this.fetchBrands();
+    },
+    methods: {
+        async fetchBrands(url) {
+            const response = url ? await this.service.fetchPaginatedData(url) : await this.service.allBrands(this.filterOptions);
+            this.brands = response.data;
+            this.paginationLinks = response.links;
+        },
+        async deleteBrand(brandId) {
+            const response = await this.service.deleteBrand(brandId);
+
+            if (response.status === 204) {
+                this.fetchBrands();
+            }
+        }
+    }
 }
 </script>
 
 <template>
     <main>
-        <h1>Admin Brands Page</h1>
+        <section>
+            <PageHeader title="Brands" :itemCount="brands.length" itemLabel="brands" />
+            <OverviewTable v-if="brands.length > 0" :headers="headers" :rows="brands"
+                @delete-row="deleteBrand" />
+            <Pagination v-if="brands.length > 0" :links="paginationLinks" @change-page="fetchBrands" />
+            <p class="mt-4" v-else>No brands found.</p>
+        </section>
     </main>
 </template>
