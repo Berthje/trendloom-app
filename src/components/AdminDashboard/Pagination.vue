93d993369abda3f26a9352415655e1fc2f63fe38
Layton Berth <laytonbrth@gmail.com>
2024-05-04 16:59:39 +0200

    refactor: Improve pagination component in AdminDashboard

--- src/components/AdminDashboard/Pagination.vue
+++ src/components/AdminDashboard/Pagination.vue
@@ -38,20 +38,20 @@ export default {
                 stroke="currentColor" class="w-5 h-5">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M6.75 15.75L3 12m0 0l3.75-3.75M3 12h18" />
             </svg>
-
             <span>
                 previous
             </span>
         </a>
         <div class="items-center hidden lg:flex gap-x-3">
-            <a v-for="(link, index) in pageLinks" :key="link.label" @click.prevent="goToPage(link.url)" class="px-2 py-1 text-sm text-blue-500 rounded-md bg-blue-100">{{ link.label }}</a>
+            <a v-for="(link, index) in pageLinks" :key="link.label" @click.prevent="goToPage(link.url)"
+                :class="['px-2 py-1 text-sm  rounded-md', { 'text-blue-500 bg-blue-100': link.active }, { 'cursor-pointer hidden sm:inline-block': !link.active }]">{{
+                link.label }}</a>
         </div>
         <a href="#" :disabled="!nextPageUrl" @click.prevent="goToPage(nextPageUrl)"
             class="flex items-center px-5 py-2 text-sm text-gray-700 capitalize transition-colors duration-200 bg-white border rounded-md gap-x-2 hover:bg-gray-100">
             <span>
                 Next
             </span>
-
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
                 stroke="currentColor" class="w-5 h-5">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M17.25 8.25L21 12m0 0l-3.75 3.75M21 12H3" />
