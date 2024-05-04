b47cbc389863fc3ce77a6f13636cb500158395b8
Layton Berth <laytonbrth@gmail.com>
2024-05-04 15:03:00 +0200

    refactor: Add DashboardHeader component to AdminDashboardPage.vue

--- src/views/AdminDashboardPage.vue
+++ src/views/AdminDashboardPage.vue
@@ -1,10 +1,12 @@
 <script>
 import DashboardSidebar from '@/components/AdminDashboard/DashboardSidebar.vue'
+import DashboardHeader from '@/components/AdminDashboard/DashboardHeader.vue'
 
 export default {
     name: 'AdminDashboardPage',
     components: {
         DashboardSidebar,
+        DashboardHeader,
     },
 }
 </script>
@@ -12,7 +14,8 @@ export default {
 <template>
     <main class="flex">
         <DashboardSidebar />
-        <div class="p-6 pl-12">
+        <div class="p-6 pl-12 flex-grow">
+            <DashboardHeader />
             <RouterView></RouterView>
         </div>
     </main>
