5d2132fee7c68e5dbe921ed188e96a719122774f
Layton Berth <laytonbrth@gmail.com>
2024-05-04 15:10:17 +0200

    feat: Add search functionality to AdminProductsPage.vue

--- src/views/AdminProductsPage.vue
+++ src/views/AdminProductsPage.vue
@@ -6,6 +6,10 @@ export default {
 
 <template>
     <main>
-        <h1>Admin Products Page</h1>
+        <div class="flex items-center space-x-6">
+            <h1 class="text-4xl font-bold">Products</h1>
+            <input type="text" placeholder="Search product based on its details..."
+                class="border border-black text-black outline-none px-2 py-1 w-full max-w-[30rem] rounded-md" />
+        </div>
     </main>
 </template>
