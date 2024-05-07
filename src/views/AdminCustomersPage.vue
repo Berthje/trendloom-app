92b9855c567902935f00dacf8c1bbfb2ae09e814
Layton Berth <laytonbrth@gmail.com>
2024-05-07 15:05:17 +0200

    refactor: Update AdminDashboard SidebarItem.vue to use dynamic link for adding items

--- src/views/AdminCustomersPage.vue
+++ src/views/AdminCustomersPage.vue
@@ -7,5 +7,6 @@ export default {
 <template>
     <main>
         <h1>Admin Customers Page</h1>
+        <p class="mt-4 text-2xl font-bold underline">THIS IS NOT PART OF MY MVP</p>
     </main>
 </template>
