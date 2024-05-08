de09ef4d9141b704e662888175a6d7537a3ffe36
Layton Berth <laytonbrth@gmail.com>
2024-05-08 13:26:34 +0200

    refactor: Improve CartMenu.vue component

--- src/components/CartMenu/CartMenu.vue
+++ src/components/CartMenu/CartMenu.vue
@@ -42,7 +42,13 @@ export default {
         },
         totalPrice() {
             return this.products.reduce((total, product) => total + product.price * product.quantity, 0);
-        }
+        },
+        isCartEmpty() {
+            return this.products.length === 0;
+        },
+        displayedProducts() {
+            return this.products.slice(0, this.MAX_PRODUCTS_SHOWN);
+        },
     },
     methods: {
         close() {
@@ -95,28 +101,27 @@ export default {
         <div v-show="show" class="fixed top-0 h-full w-full overflow-auto z-50">
             <nav class="fixed top-0 right-0 h-full w-[22rem] bg-white overflow-auto p-8 text-center flex flex-col">
                 <CartMenuHeader :amountOfProducts="itemCount" />
-                <div v-if="products.length === 0">
+                <div v-if="isCartEmpty">
                     <p class="text-sm mb-4">{{ $t('no_products_in_cart') }}</p>
-                    <RouterLink @click="$emit('close')" to="/shop"
+                    <RouterLink @click="close" to="/shop"
                         class="inline-block px-4 py-2 border border-solid border-black uppercase text-sm hover:bg-black hover:text-white transition-all duration-150">
                         {{ $t('return_to_shop') }}
                     </RouterLink>
                 </div>
                 <div v-else class="flex flex-grow">
                     <ul class="overflow-y-auto flex-grow">
-                        <ProductItem v-for="(product, index) in products.slice(0, MAX_PRODUCTS_SHOWN)" :key="product.id"
-                            :product="product" @close="close" @remove="removeProductFromCart" />
+                        <ProductItem v-for="(product, index) in displayedProducts" :key="product.id" :product="product"
+                            @close="close" @remove="removeProductFromCart" />
                     </ul>
                 </div>
                 <p v-if="additionalProducts > 0" class="text-left mb-32 font-bold">
-                    ..{{ $t('there') }} {{ additionalProducts === 1 ? $t('is') : $t('are') }} {{ additionalProducts
-                    }}
+                    ..{{ $t('there') }} {{ additionalProducts === 1 ? $t('is') : $t('are') }} {{ additionalProducts }}
                     {{ $t('more') }}
                     {{
                         additionalProducts > 1 ? $t('products') :
                             $t('product') }}
                 </p>
-                <CartMenuFooter v-if="products.length > 0" :subtotal="totalPrice" />
+                <CartMenuFooter v-if="!isCartEmpty" :subtotal="totalPrice" />
             </nav>
             <AiOutlineClose v-show="show"
                 class="h-8 w-8 bg-white rounded-full p-2 cursor-pointer z-50 absolute right-[23rem] top-3"
